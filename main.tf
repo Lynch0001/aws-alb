47dd53ba805de2c095cdb2b43721ed8382426ec0
Tim Lynch <Lynch0001@gmail.com>
2024-05-29 15:36:24 -0400

    asg

--- main.tf
+++ main.tf
@@ -174,7 +174,7 @@ resource "aws_lb_target_group_attachment" "this" {
 resource "aws_lb_target_group_attachment" "additional" {
 for_each = { for k, v in var.additional_target_group_attachments : k => v if local.create_lb }
 
-target_group_arn  = aws_lb_target_group.main[each.value.target_group_index].arn
+target_group_arn  = aws_lb_target_group.main[each.value.tg_index].arn
 target_id         = each.value.target_id
 port              = each.value.port
 availability_zone = try(each.value.availability_zone, null)

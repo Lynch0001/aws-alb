5d9e77fe9636d5ddee21b75de3c9bba8fa205f8b
Tim Lynch <Lynch0001@gmail.com>
2024-08-03 12:26:41 -0400

    merge asg attachments

--- main.tf
+++ main.tf
@@ -191,12 +191,36 @@ availability_zone = try(each.value.availability_zone, null)
 }
 
 
-resource "aws_autoscaling_attachment" "asg_attach" {
-  for_each      = { for entry in local.asg_attach_data: "${entry.autoscaling_group}.${entry.target_group}" => entry if var.attach_asg }
 
-  autoscaling_group_name = each.value.autoscaling_group.id
-  lb_target_group_arn   = aws_lb_target_group.main[each.value.target_group.index].arn
+if length(var.autoscaling_groups)==3{
+
+  resource "aws_autoscaling_attachment" "asg1" {
+  for_each = {for k, v in var.target_groups : k => v if var.attach_asg}
+
+  autoscaling_group_name = var.autoscaling_groups[0]
+  lb_target_group_arn = aws_lb_target_group.main[each.key].arn
+  }
+
+  resource "aws_autoscaling_attachment" "asg2" {
+  for_each = {for k, v in var.target_groups : k => v if var.attach_asg}
+
+  autoscaling_group_name = var.autoscaling_groups[1]
+  lb_target_group_arn = aws_lb_target_group.main[each.key].arn
+  }
+
+  resource "aws_autoscaling_attachment" "asg3" {
+  for_each = {for k, v in var.target_groups : k => v if var.attach_asg}
+
+  autoscaling_group_name = var.autoscaling_groups[2]
+  lb_target_group_arn = aws_lb_target_group.main[each.key].arn
+  }
 }
+
+
+
+
+
+
 
 resource "aws_lb_listener_rule" "https_listener_rule" {
   count = local.create_lb ? length(var.https_listener_rules) : 0
